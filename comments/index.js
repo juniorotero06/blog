076f80049e44f85478a23fe0623c4e35a3d64e9e
Junior Otero <dgrotero06@gmail.com>
2023-05-17 22:36:40 -0500

    first commit

---
+++ comments/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", async (req, res) => {
+  const commnetId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({ id: commnetId, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commnetId,
+      content,
+      postId: req.params.id,
+    },
+  });
+
+  res.status(201).send(comments);
+});
+
+app.post("/events", async (req, res) => {
+  console.log("Event Received: ", req.body.type);
+  res.send({});
+});
+
+app.listen(4001, () => {
+  console.log("Listen in port 4001");
+});
