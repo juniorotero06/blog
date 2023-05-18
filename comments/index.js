b61ebbfd1254dfdce34742f0ebfc7b045fcd479e
JuniorOtero <dgrotero697@gmail.com>
2023-05-18 11:41:28 -0500

    moderated and updated comment

--- comments/index.js
+++ comments/index.js
@@ -38,6 +38,27 @@ app.post("/posts/:id/comments", async (req, res) => {
 
 app.post("/events", async (req, res) => {
   console.log("Event Received: ", req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === "CommentModerated"){
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((c) => c.id === id);
+    comment.status = status;
+    
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        content,
+        status,
+        postId
+      },
+    });
+  }
+
   res.send({});
 });
 
--- query/index.js
+++ query/index.js
@@ -25,6 +25,15 @@ app.post("/events", async (req, res) => {
     post.comments.push({ id, content, status });
   }
 
+  if ( type === "CommentUpdated") {
+    const { id, content, postId, status} = data;
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => comment.id === id); 
+
+    comment.status = status;
+    comment.content = content;
+  }
+  
   console.log(posts);
 
   res.send({});
