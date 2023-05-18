a0d3b2994c3eb9eadc293a4e42046e56c866905d
JuniorOtero <dgrotero697@gmail.com>
2023-05-18 13:33:35 -0500

    cambio en commentList component

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -3,7 +3,21 @@ import React from "react";
 
 const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+
+    if (comment.status === "aproved"){
+      content = comment.content;
+    }
+
+    if (comment.status === "pending"){
+      content = "this comment is awaiting moderation";
+    }
+
+    if (comment.status === "rejected"){
+      content = "this comment has been rejected";
+    }
+
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
